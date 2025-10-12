60f29e0647c2d13c6de9a1be1045d8b35a6e5789
jakekeech <jakekeech@gmail.com>
2025-10-12 22:56:00 +0800

    Add tests for list appointment command

---
+++ src/test/java/seedu/address/logic/commands/ListAppointmentsCommandTest.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListAppointmentsCommand.
+ */
+public class ListAppointmentsCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listAppointments_showsAppointments() {
+        CommandResult expectedCommandResult = new CommandResult(
+                ListAppointmentsCommand.MESSAGE_SUCCESS, false, false, true, false);
+        assertCommandSuccess(new ListAppointmentsCommand(), model, expectedCommandResult, expectedModel);
+    }
+}
